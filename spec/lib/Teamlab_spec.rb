4b2b9d6c080ec0ae07c289f54d62268153ac8374
rzagudaev <roman-zagudaev@yandex.ru>
2014-05-13 15:24:56 +0400

    Completed Mail module library

--- spec/lib/Teamlab_spec.rb
+++ spec/lib/Teamlab_spec.rb
@@ -3585,11 +3585,46 @@ describe Teamlab do
       end
     end
 
+    describe '#create_account_by_email' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :create_account_by_email }
+        let(:args) { [USERNAME, PASSWORD] }
+      end
+    end
+
+
+
 
 
 ########################################################################################################################
 ############################################# END CREATING #############################################################
 ########################################################################################################################
+
+    describe '#get_filtered_messages' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :get_filtered_messages }
+        let(:args) { [2] }
+      end
+    end
+
+    describe '#get_message' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :get_message }
+        let(:args) {message_id}
+      end
+    end
+
+    describe '#get_message_template' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :get_message_template }
+      end
+    end
+
+    describe '#get_account_list' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :get_account_list }
+      end
+    end
 
     describe '#get_tag_list' do
       it_should_behave_like 'an api request' do
