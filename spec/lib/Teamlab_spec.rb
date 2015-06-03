8fdbb34196eafe1515cbee4e3a21f7899289cfe0
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-03 16:17:27 +0300

    move shared_example to spec_helper

--- spec/lib/Teamlab_spec.rb
+++ spec/lib/Teamlab_spec.rb
@@ -10,38 +10,6 @@ describe Teamlab do
     end
   end
 
-  shared_examples_for 'an api request' do |*flags|
-    before { pending } if flags.include?(:pending)
-    before do
-      @module = Teamlab.send(teamlab_module)
-      puts "#{command}(#{args.join(', ')})"
-      @response = args.empty? ? @module.send(command) : @module.send(command, *args)
-      if add_data_to_collector
-        DATA_COLLECTOR[data_param] ||= []
-        DATA_COLLECTOR[data_param] << param_names.inject(@response.body['response']) { |resp, param| resp[param] }
-      end
-    end
-
-    context 'Successful api request' do
-
-      it 'returns Teamlab::Response object' do
-        expect(@response).to be_instance_of(Teamlab::Response)
-      end
-
-      it 'returns Teamlab::Response object with successful parameter, set to true' do
-        expect(@response.success).to be_truthy
-      end
-
-      it 'returns Teamlab::Response object with nil error parameter' do
-        expect(@response.error).to be_nil
-      end
-
-      it 'returns Teamlab::Response object with hash body' do
-        expect(@response.body).to be_instance_of(Hash)
-      end
-    end
-  end
-
   describe 'Preparing enviroment' do
     describe '#add_user' do
       it_should_behave_like 'an api request' do
