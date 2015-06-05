39bf9d34c53cedfef67c0457b2e0a4f41db0ba43
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-05 18:41:53 +0300

    parse response if returned a string respsone

--- lib/teamlab/Response.rb
+++ lib/teamlab/Response.rb
@@ -5,12 +5,17 @@ module Teamlab
     attr_reader :body, :error, :code, :success
 
     def initialize(http_response)
-      @code = http_response.code
-      @success = @code < 400
-      fail 'Error 404' if @code == 404
-      fail TimeoutError, 'Portal is warming up' if http_response.parsed_response.include?('portal is being warmed')
-      @body = http_response.respond_to?(:parsed_response) && http_response.parsed_response.key?('result') ? http_response.parsed_response['result'] : http_response.to_hash
-      @error = @body['error']['message'] if @body.key?('error') && @body['error'].key?('message')
+      if http_response.is_a?(String)
+        @body = {:'response' => http_response.to_s}
+        @success = true
+        @code = 201
+      else
+        @code = http_response.code
+        @success = @code < 400
+        fail 'Error 404' if @code == 404
+        fail TimeoutError, 'Portal is warming up' if http_response.parsed_response.include?('portal is being warmed')
+        @body = http_response.respond_to?(:parsed_response) && http_response.parsed_response.key?('result') ? http_response.parsed_response['result'] : http_response.to_hash
+        @error = @body['error']['message'] if @body.key?('error') && @body['error'].key?('message')      end
     end
   end
 end
