e6930f4b86cb81b89d6bcb3a4c449220a05dfd8d
Pavel Lobashov <shockwavenn@gmail.com>
2015-12-24 19:26:54 +0300

    Fix rubocop Lint/UnusedMethodArgument cop

--- lib/teamlab/config.rb
+++ lib/teamlab/config.rb
@@ -5,7 +5,7 @@ require_relative 'request'
 module Teamlab
   attr_reader :config
 
-  def self.configure(&block)
+  def self.configure(&_block)
     @config ||= Config.new
     yield @config if block_given?
     auth_response = Teamlab::Request.new('authentication').post('', userName: @config.username, password: @config.password).body
