26fdbd6ff94d898208b1174835303f1258870085
Pavel Lobashov <shockwavenn@gmail.com>
2018-12-12 18:09:56 +0300

    Remove users before running test
    Fix problem with only 30 users are allowed on portal

--- spec/spec_helper/portal_cleanup.rb
+++ spec/spec_helper/portal_cleanup.rb
@@ -2,12 +2,24 @@ module PortalCleanup
   # @return [Void] reset all portal data
   def reset_portal
     reset_modules
+    remove_users
   end
 
   # @return [Void] Set all modules to true
   def reset_modules
     SETTINGS_ENTITY_IDS.each do |module_id|
       Teamlab.settings.set_access(module_id, true)
+    end
+  end
+
+  # @return [Void] Remove all user expect admin from portal
+  def remove_users
+    users = Teamlab.people.get_people.body['response']
+    users.each do |user|
+      next if user['isOwner']
+
+      Teamlab.people.change_people_status('2', [user['id']])
+      Teamlab.people.delete_user(user['id'])
     end
   end
 end
