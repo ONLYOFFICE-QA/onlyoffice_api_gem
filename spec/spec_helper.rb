cbeffd9dfce231306d72c5244a822c4363103921
Pavel Lobashov <ShockwaveNN@gmail.com>
2018-12-13 15:58:13 +0300

    Do not perform serveral request for request test (#77)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ end
 require 'rspec'
 require 'onlyoffice_api'
 require_relative 'spec_helper/portal_cleanup'
+require_relative 'spec_helper/request_helper'
 require_relative 'support/http_data'
 
 def configure_test_portal
@@ -28,33 +29,39 @@ end
 shared_examples_for 'an api request' do |*flags|
   before { pending } if flags.include?(:pending)
   before do
-    @module = Teamlab.send(teamlab_module)
-    puts "#{command}(#{args.join(', ')})"
-    @response = args.empty? ? @module.send(command) : @module.send(command, *args)
-    if add_data_to_collector
-      DATA_COLLECTOR[data_param] ||= []
-      response = [@response.body['response']].flatten
-      response.each do |cur_response|
-        DATA_COLLECTOR[data_param] << param_names.inject(cur_response) { |a, e| a[e] }
+    if RequestHelper.current_responce.nil?
+      @module = Teamlab.send(teamlab_module)
+      puts "#{command}(#{args.join(', ')})"
+      RequestHelper.current_responce = args.empty? ? @module.send(command) : @module.send(command, *args)
+      if add_data_to_collector
+        DATA_COLLECTOR[data_param] ||= []
+        response = [RequestHelper.current_responce.body['response']].flatten
+        response.each do |cur_response|
+          DATA_COLLECTOR[data_param] << param_names.inject(cur_response) { |a, e| a[e] }
+        end
       end
     end
   end
 
   context 'Successful api request' do
     it 'returns Teamlab::Response object' do
-      expect(@response).to be_instance_of(Teamlab::Response)
+      expect(RequestHelper.current_responce).to be_instance_of(Teamlab::Response)
     end
 
     it 'returns Teamlab::Response object with successful parameter, set to true' do
-      expect(@response.success).to be_truthy
+      expect(RequestHelper.current_responce.success).to be_truthy
     end
 
     it 'returns Teamlab::Response object with nil error parameter' do
-      expect(@response.error).to be_nil
+      expect(RequestHelper.current_responce.error).to be_nil
     end
 
     it 'returns Teamlab::Response object with hash body' do
-      expect(@response.body).to be_instance_of(Hash)
+      expect(RequestHelper.current_responce.body).to be_instance_of(Hash)
     end
+  end
+
+  after(:all) do
+    RequestHelper.current_responce = nil
   end
 end
