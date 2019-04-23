44a129981901aae1bcb2eee24d57cf877897509a
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-04-23 19:37:06 +0300

    Extract crm history methods (#109)
    * Extract crm history methods
    
    * Update rubocop

--- lib/teamlab/modules/crm.rb
+++ lib/teamlab/modules/crm.rb
@@ -2,6 +2,7 @@ require_relative 'crm/crm_cases'
 require_relative 'crm/crm_common'
 require_relative 'crm/crm_contacts'
 require_relative 'crm/crm_files'
+require_relative 'crm/crm_history'
 require_relative 'crm/crm_invoices'
 module Teamlab
   class Crm
@@ -9,6 +10,7 @@ module Teamlab
     include CrmCommon
     include CrmContacts
     include CrmFiles
+    include CrmHistory
     include CrmInvoices
 
     def initialize
@@ -237,42 +239,6 @@ module Teamlab
 
     def delete_invoice_line(id)
       @request.delete(['invoiceline', id.to_s])
-    end
-
-    def get_event_list_by_filter(options = {})
-      @request.get(%w[history filter], options)
-    end
-
-    def get_all_history_categories
-      @request.get(%w[history category])
-    end
-
-    def create_event(contact_id, content, category_id, options = {})
-      @request.post(%w[history], { contactId: contact_id, content: content, categoryId: category_id }.merge(options))
-    end
-
-    def create_history_category(title, image_name, options = {})
-      @request.post(%w[history category], { title: title.to_s, imageName: image_name.to_s }.merge(options))
-    end
-
-    def update_history_category(id, title, options = {})
-      @request.put(['history', 'category', id.to_s], { title: title }.merge(options))
-    end
-
-    def update_history_categories_order(*titles)
-      @request.put(%w[history category reorder], titles: titles.flatten)
-    end
-
-    def update_history_category_icon(id, icon_name)
-      @request.put(['history', 'category', id.to_s, 'icon'], imageName: icon_name.to_s)
-    end
-
-    def delete_event_and_related_files(id)
-      @request.delete(['history', id.to_s])
-    end
-
-    def delete_history_category(id)
-      @request.delete(['history', 'category', id.to_s])
     end
 
     def get_task_list_by_filter(options = {})
---
+++ lib/teamlab/modules/crm/crm_history.rb
@@ -0,0 +1,40 @@
+module Teamlab
+  # Methods for working with crm history
+  module CrmHistory
+    def get_event_list_by_filter(options = {})
+      @request.get(%w[history filter], options)
+    end
+
+    def get_all_history_categories
+      @request.get(%w[history category])
+    end
+
+    def create_event(contact_id, content, category_id, options = {})
+      @request.post(%w[history], { contactId: contact_id, content: content, categoryId: category_id }.merge(options))
+    end
+
+    def create_history_category(title, image_name, options = {})
+      @request.post(%w[history category], { title: title.to_s, imageName: image_name.to_s }.merge(options))
+    end
+
+    def update_history_category(id, title, options = {})
+      @request.put(['history', 'category', id.to_s], { title: title }.merge(options))
+    end
+
+    def update_history_categories_order(*titles)
+      @request.put(%w[history category reorder], titles: titles.flatten)
+    end
+
+    def update_history_category_icon(id, icon_name)
+      @request.put(['history', 'category', id.to_s, 'icon'], imageName: icon_name.to_s)
+    end
+
+    def delete_event_and_related_files(id)
+      @request.delete(['history', id.to_s])
+    end
+
+    def delete_history_category(id)
+      @request.delete(['history', 'category', id.to_s])
+    end
+  end
+end
