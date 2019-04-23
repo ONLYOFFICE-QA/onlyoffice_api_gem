44615e432c3cd5e2a12aca14ca99de840e1efbdb
Pavel Lobashov <shockwavenn@gmail.com>
2019-04-23 16:15:36 +0300

    Fix users tests

--- spec/lib/crm/crm_contacts_spec.rb
+++ spec/lib/crm/crm_contacts_spec.rb
@@ -3,6 +3,17 @@ require 'spec_helper'
 describe '[CRM]' do
   let(:teamlab_module) { :crm }
 
+  describe '#add_user' do
+    it_should_behave_like 'an api request' do
+      let(:teamlab_module) { :people }
+      let(:command) { :add_user }
+      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
+      let(:add_data_to_collector) { true }
+      let(:data_param) { :user_ids }
+      let(:param_names) { %w[id] }
+    end
+  end
+
   describe '#create_contact_type' do
     it_should_behave_like 'an api request' do
       let(:command) { :create_contact_type }
