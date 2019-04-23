61bd89c754fafa7934735f311a9dc93cdfe5e244
Pavel Lobashov <shockwavenn@gmail.com>
2019-04-23 18:48:08 +0300

    Update rubocop

--- lib/teamlab/modules/crm/crm_files.rb
+++ lib/teamlab/modules/crm/crm_files.rb
@@ -6,19 +6,33 @@ module Teamlab
     end
 
     def get_file_list(entity_type, entity_id)
-      @request.get([entity_type.to_s, entity_id.to_s, 'files'])
+      @request.get([entity_type.to_s,
+                    entity_id.to_s,
+                    'files'])
     end
 
     def associate_file_with_entity(entity_type, entity_id, *fileids)
-      @request.post([entity_type, entity_id, 'files'], fileids: fileids.flatten)
+      @request.post([entity_type,
+                     entity_id,
+                     'files'],
+                    fileids: fileids.flatten)
     end
 
     def create_txt(entity_type, entity_id, title, content)
-      @request.post([entity_type.to_s, entity_id.to_s, 'files', 'text'], title: title, content: content)
+      @request.post([entity_type.to_s,
+                     entity_id.to_s,
+                     'files',
+                     'text'],
+                    title: title,
+                    content: content)
     end
 
     def upload_file(entity_type, entity_id, file)
-      @request.post([entity_type.to_s, entity_id.to_s, 'files', 'upload'], somefile: File.new(file))
+      @request.post([entity_type.to_s,
+                     entity_id.to_s,
+                     'files',
+                     'upload'],
+                    somefile: File.new(file))
     end
 
     def delete_file(id)
