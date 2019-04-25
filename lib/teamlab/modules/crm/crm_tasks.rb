505d70032f17673595063560427941978c516a11
Pavel Lobashov <shockwavenn@gmail.com>
2019-04-25 14:02:01 +0300

    Extract crm tasks to separate file

---
+++ lib/teamlab/modules/crm/crm_tasks.rb
@@ -0,0 +1,60 @@
+module Teamlab
+  # Methods for working with crm tasks
+  module CrmTasks
+    def create_task(title, deadline, responsible_id, category_id, options = {})
+      @request.post(%w[task], { title: title, deadline: deadline, responsibleId: responsible_id, categoryId: category_id }.merge(options))
+    end
+
+    def update_task(task_id, title, deadline, category_id, options = {})
+      @request.put(['task', task_id.to_s], { title: title, deadline: deadline, categoryid: category_id }.merge(options))
+    end
+
+    def get_task_list_by_filter(options = {})
+      @request.get(%w[task filter], options)
+    end
+
+    def get_all_task_categories
+      @request.get(%w[task category])
+    end
+
+    def get_task_by_id(task_id)
+      @request.get(['task', task_id.to_s])
+    end
+
+    def get_task_category(category_id)
+      @request.get(['task', 'category', category_id.to_s])
+    end
+
+    def create_task_category(title, image_name, options = {})
+      @request.post(%w[task category], { title: title.to_s, imageName: image_name.to_s }.merge(options))
+    end
+
+    def update_task_category(category_id, title, options = {})
+      @request.put(['task', 'category', category_id.to_s], { title: title }.merge(options))
+    end
+
+    def close_task(task_id)
+      @request.put(['task', task_id.to_s, 'close'])
+    end
+
+    def resume_task(task_id)
+      @request.put(['task', task_id.to_s, 'reopen'])
+    end
+
+    def update_task_categories_order(*titles)
+      @request.put(%w[task category reorder], titles: titles.flatten)
+    end
+
+    def update_task_category_icon(id, image_name)
+      @request.put(['task', 'category', id.to_s, 'icon'], imageName: image_name.to_s)
+    end
+
+    def delete_task(id)
+      @request.delete(['task', id.to_s])
+    end
+
+    def delete_task_category(category_id)
+      @request.delete(['task', 'category', category_id.to_s])
+    end
+  end
+end
---
+++ spec/lib/crm/crm_tasks_spec.rb
@@ -0,0 +1,122 @@
+require 'spec_helper'
+
+describe '[CRM]' do
+  let(:teamlab_module) { :crm }
+
+  describe '#add_user' do
+    it_should_behave_like 'an api request' do
+      let(:teamlab_module) { :people }
+      let(:command) { :add_user }
+      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
+      let(:add_data_to_collector) { true }
+      let(:data_param) { :user_ids }
+      let(:param_names) { %w[id] }
+    end
+  end
+
+  describe '#create_task_category' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :create_task_category }
+      let(:args) { [random_word, random_word] }
+      let(:add_data_to_collector) { true }
+      let(:data_param) { :crm_task_category_ids }
+      let(:param_names) { %w[id] }
+    end
+  end
+
+  describe '#update_task_category' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :update_task_category }
+      i = -1
+      let(:args) { [DATA_COLLECTOR[:crm_task_category_ids][i += 1], random_word] }
+      let(:add_data_to_collector) { true }
+      let(:data_param) { :crm_task_category_titles }
+      let(:param_names) { %w[title] }
+    end
+  end
+
+  describe '#create_task' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :create_task }
+      let(:args) { [random_word, DateTime.commercial(2015).to_s, random_id(:user), random_id(:crm_task_category)] }
+      let(:add_data_to_collector) { true }
+      let(:data_param) { :crm_task_ids }
+      let(:param_names) { %w[id] }
+    end
+  end
+
+  describe '#update_task' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :update_task }
+      let(:args) { [random_id(:crm_task), random_word, DateTime.commercial(2015).to_s, random_id(:crm_task_category)] }
+    end
+  end
+
+  describe '#get_task_list_by_filter' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :get_task_list_by_filter }
+    end
+  end
+
+  describe '#get_all_task_categories' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :get_all_task_categories }
+    end
+  end
+
+  describe '#get_task_by_id' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :get_task_by_id }
+      let(:args) { [random_id(:crm_task)] }
+    end
+  end
+
+  describe '#get_task_category' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :get_task_category }
+      let(:args) { [random_id(:crm_task_category)] }
+    end
+  end
+
+  describe '#close_task' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :close_task }
+      let(:args) { [random_id(:crm_task)] }
+    end
+  end
+
+  describe '#resume_task' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :resume_task }
+      let(:args) { [random_id(:crm_task)] }
+    end
+  end
+
+  describe '#update_task_categories_order' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :update_task_categories_order }
+      let(:args) { [DATA_COLLECTOR[:crm_task_category_titles].shuffle] }
+    end
+  end
+
+  describe '#update_task_category_icon' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :update_task_category_icon }
+      let(:args) { [random_id(:crm_task_category), random_word] }
+    end
+  end
+
+  describe '#delete_task' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :delete_task }
+      let(:args) { [DATA_COLLECTOR[:crm_task_ids].pop] }
+    end
+  end
+
+  describe '#delete_task_category' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :delete_task_category }
+      let(:args) { [DATA_COLLECTOR[:crm_task_category_ids].pop] }
+    end
+  end
+end
