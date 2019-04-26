7c971ed5da416ee8150f1c2cffa390c3a00e9c32
Pavel Lobashov <shockwavenn@gmail.com>
2019-04-26 14:36:34 +0300

    Extract project comments to module spec

--- lib/teamlab/modules/project.rb
+++ lib/teamlab/modules/project.rb
@@ -1,6 +1,8 @@
+require_relative 'projects/projects_comments'
 require_relative 'projects/projects_settings'
 module Teamlab
   class Project
+    include ProjectsComments
     include ProjectsSettings
 
     def initialize
@@ -96,38 +98,6 @@ module Teamlab
 
     def detach_file_from_message(message_id, file_id)
       @request.delete(['message', message_id.to_s, 'files'], fileid: file_id)
-    end
-
-    # endregion
-
-    # region Comment
-
-    def get_comment(comment_id)
-      @request.get(['comment', comment_id.to_s])
-    end
-
-    def get_task_comments(task_id)
-      @request.get(['task', task_id.to_s, 'comment'])
-    end
-
-    def get_message_comments(message_id)
-      @request.get(['message', message_id.to_s, 'comment'])
-    end
-
-    def add_task_comment(task_id, content, options = {})
-      @request.post(['task', task_id.to_s, 'comment'], { content: content }.merge(options))
-    end
-
-    def add_message_comment(message_id, content, options = {})
-      @request.post(['message', message_id.to_s, 'comment'], { content: content }.merge(options))
-    end
-
-    def update_comment(comment_id, content)
-      @request.put(['comment', comment_id.to_s], content: content)
-    end
-
-    def delete_comment(comment_id)
-      @request.delete(['comment', comment_id.to_s])
     end
 
     # endregion
