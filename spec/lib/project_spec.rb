bc799f1f6d4ca956e38c3de56c3338429e81de79
Pavel Lobashov <shockwavenn@gmail.com>
2019-05-08 14:21:19 +0300

    Remove all duplicate methods

--- spec/lib/project_spec.rb
+++ spec/lib/project_spec.rb
@@ -3,49 +3,6 @@ require_relative '../spec_helper'
 describe '[Project]' do
   let(:teamlab_module) { :project }
 
-  describe '#add_user' do
-    it_should_behave_like 'an api request' do
-      let(:teamlab_module) { :people }
-      let(:command) { :add_user }
-      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_project' do
-    it_should_behave_like 'an api request' do
-      let(:teamlab_module) { :project }
-      let(:command) { :create_project }
-      let(:args) { [random_word, random_word, random_id(:user), random_word(3), random_bool] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :project_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#get_my_files' do
-    it_should_behave_like 'an api request' do
-      let(:teamlab_module) { :files }
-      let(:command) { :get_my_files }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :my_documents_ids }
-      let(:param_names) { %w[current id] }
-    end
-  end
-
-  describe '#create_file' do
-    it_should_behave_like 'an api request' do
-      let(:teamlab_module) { :files }
-      let(:command) { :create_file }
-      let(:args) { [random_id(:my_documents), random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :file_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
   describe '#add_importing_url_to_queue' do
     it_should_behave_like 'an api request' do
       let(:command) { :add_importing_url_to_queue }
@@ -63,26 +20,6 @@ describe '[Project]' do
   describe '#get_import_status' do
     it_should_behave_like 'an api request' do
       let(:command) { :get_import_status }
-    end
-  end
-
-  describe '#create_project' do
-    it_should_behave_like 'an api request' do
-      let(:command) { :create_project }
-      let(:args) { [random_word, random_word, random_id(:user), random_word(3), random_bool, { participants: DATA_COLLECTOR[:user_ids] }] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :new_project_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#add_task' do
-    it_should_behave_like 'an api request' do
-      let(:command) { :add_task }
-      let(:args) { [random_id(:new_project), random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :project_task_ids }
-      let(:param_names) { %w[id] }
     end
   end
 end
