b85835ff92d28764b47727dc1370170493d2535c
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-05-17 18:29:59 +0300

    Add methods to People Reassign (#125)

---
+++ spec/lib/people/people_reassign_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe '[People] Reassign' do
+  let(:teamlab_module) { :people }
+
+  describe 'Preconditions' do
+    describe '#add_user' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :add_user }
+        let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
+        let(:add_data_to_collector) { true }
+        let(:data_param) { :user_ids }
+        let(:param_names) { %w[id] }
+      end
+    end
+
+    describe '#add_user' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :add_user }
+        let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
+        let(:add_data_to_collector) { true }
+        let(:data_param) { :user_ids }
+        let(:param_names) { %w[id] }
+      end
+    end
+
+    describe '#change_people_status' do
+      it_should_behave_like 'an api request' do
+        let(:command) { :change_people_status }
+        let(:args) { [2, DATA_COLLECTOR[:user_ids].first] }
+      end
+    end
+  end
+
+  describe '#start_reassign_user_data' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :start_reassign_user_data }
+      let(:args) { [DATA_COLLECTOR[:user_ids].first, DATA_COLLECTOR[:user_ids].last, false] }
+    end
+  end
+
+  describe '#get_reassign_progress' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :get_reassign_progress }
+      let(:args) { [DATA_COLLECTOR[:user_ids].first] }
+    end
+  end
+
+  describe '#terminate_reassign' do
+    it_should_behave_like 'an api request' do
+      let(:command) { :terminate_reassign }
+      let(:args) { [DATA_COLLECTOR[:user_ids].first] }
+    end
+  end
+end
