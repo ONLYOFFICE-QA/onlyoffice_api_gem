ce95e46f2d9d520cd03418a1ca1a94051ef03779
Pavel Lobashov <shockwavenn@gmail.com>
2021-07-09 18:46:02 +0300

    Fix more

--- lib/onlyoffice_api/onlyoffice_api_instance.rb
+++ lib/onlyoffice_api/onlyoffice_api_instance.rb
@@ -1,61 +1,65 @@
 # frozen_string_literal: true
 
-# Class for multiuser instance
-class OnlyofficeApiInstance
-  def initialize(params = {})
-    @config = Config.new(params)
-    auth_response = Teamlab::Request.new('authentication').post('', userName: @config.username, password: @config.password).body
-    raise "Cannot get response token for #{auth_response}" if auth_response['response'].nil? || auth_response['response']['token'].nil?
+require_relative '../teamlab/config'
 
-    @config.token = auth_response['response']['token']
-    @config.headers = { 'authorization' => @config.token }
-  end
+module Teamlab
+  # Class for multiuser instance
+  class OnlyofficeApiInstance
+    def initialize(params = {})
+      @config = Config.new(params)
+      auth_response = Teamlab::Request.new('authentication').post('', userName: @config.username, password: @config.password).body
+      raise "Cannot get response token for #{auth_response}" if auth_response['response'].nil? || auth_response['response']['token'].nil?
 
-  def people
-    @people ||= Teamlab::People.new
-  end
+      @config.token = auth_response['response']['token']
+      @config.headers = { 'authorization' => @config.token }
+    end
 
-  def group
-    @group ||= Teamlab::Group.new
-  end
+    def people
+      @people ||= Teamlab::People.new
+    end
 
-  def settings
-    @settings ||= Teamlab::Settings.new
-  end
+    def group
+      @group ||= Teamlab::Group.new
+    end
 
-  def files
-    @files ||= Teamlab::Files.new
-  end
+    def settings
+      @settings ||= Teamlab::Settings.new
+    end
 
-  def project
-    @project ||= Teamlab::Project.new
-  end
+    def files
+      @files ||= Teamlab::Files.new
+    end
 
-  def portal
-    @portal ||= Teamlab::Portal.new
-  end
+    def project
+      @project ||= Teamlab::Project.new
+    end
 
-  def crm
-    @crm ||= Teamlab::Crm.new
-  end
+    def portal
+      @portal ||= Teamlab::Portal.new
+    end
 
-  def community
-    @community ||= Teamlab::Community.new
-  end
+    def crm
+      @crm ||= Teamlab::Crm.new
+    end
 
-  def calendar
-    @calendar ||= Teamlab::Calendar.new
-  end
+    def community
+      @community ||= Teamlab::Community.new
+    end
 
-  def mail
-    @mail ||= Teamlab::Mail.new
-  end
+    def calendar
+      @calendar ||= Teamlab::Calendar.new
+    end
 
-  def mailserver
-    @mailserver ||= Teamlab::MailServer.new
-  end
+    def mail
+      @mail ||= Teamlab::Mail.new
+    end
 
-  def feed
-    @feed ||= Teamlab::Feed.new
+    def mailserver
+      @mailserver ||= Teamlab::MailServer.new
+    end
+
+    def feed
+      @feed ||= Teamlab::Feed.new
+    end
   end
 end
--- spec/spec_helper/portal_cleanup/crm_cleanup.rb
+++ spec/spec_helper/portal_cleanup/crm_cleanup.rb
@@ -2,27 +2,30 @@
 
 # Module for code to cleanup crm
 module CrmCleanup
+  # @param [Teamlab::OnlyofficeApiInstance] api to use
   # Remove all data from crm
-  def crm_cleanup
-    remove_invoices
-    remove_contacts
+  def crm_cleanup(api)
+    remove_invoices(api)
+    remove_contacts(api)
   end
 
+  # @param [Teamlab::OnlyofficeApiInstance] api to use
   # Remove all crm invoices
-  def remove_invoices
-    invoices = Teamlab.crm.get_invoices_by_filter.body['response']
+  def remove_invoices(api)
+    invoices = api.crm.get_invoices_by_filter.body['response']
     invoices_ids = invoices.map { |invoice| invoice['id'] }
     return if invoices_ids.empty?
 
-    Teamlab.crm.delete_batch_invoices(invoices_ids)
+    api.crm.delete_batch_invoices(invoices_ids)
   end
 
-  # Remove all crm contacts
+  # @param [Teamlab::OnlyofficeApiInstance] api to use
+  # Remove all crm contacts(api)
   def remove_contacts
-    contacts = Teamlab.crm.get_contacts_by_filter.body['response']
+    contacts = api.crm.get_contacts_by_filter.body['response']
     contact_ids = contacts.map { |contact| contact['id'] }
     return if contact_ids.empty?
 
-    Teamlab.crm.delete_contact_group(contact_ids)
+    api.crm.delete_contact_group(contact_ids)
   end
 end
