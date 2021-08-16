8c10ede673117fe6a6443128b7fd3b776b2e09d7
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-08-16 17:49:04 +0300

    People/Add api methods for user removing (#310)
    * People/Add api methods for user removing
    
    * Documentation fix
    
    * Delete get

---
+++ lib/teamlab/modules/people/people_remove.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Teamlab
+  # Module for people removing process
+  module PeopleRemove
+    # Start a remove process
+    # @param userId [String] guid of user to remove
+    # @return [Hash] remove progress data
+    def start_remove(user_id)
+      @request.post(%w[remove start], userId: user_id)
+    end
+
+    # Returns the progress of the started remove process
+    # @param userId [String] guid of user to remove
+    # @return [Hash] remove progress data
+    def remove_progress(user_id)
+      @request.get(%w[remove progress], userId: user_id)
+    end
+
+    # Terminate remove process
+    # @param userId [String] guid of user to remove
+    # @return [Void]
+    def terminate_remove(user_id)
+      @request.put(%w[remove terminate], userId: user_id)
+    end
+  end
+end
