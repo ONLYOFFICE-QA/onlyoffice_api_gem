565e6dbf206d0e6d6e9ee2bd341b311052e99cdd
Olga Nesterova <olganesterova034@gmail.com>
2021-08-17 16:44:28 +0300

    Refactoring CRM Cases API methods spec

--- spec/lib/crm/crm_cases_spec.rb
+++ spec/lib/crm/crm_cases_spec.rb
@@ -2,54 +2,18 @@
 
 require 'spec_helper'
 
+crm_case_user = api.crm.create_person(random_word.capitalize, random_word.capitalize).data
+crm_case = api.crm.create_case(random_word, { members: crm_case_user['id'] }).data
+crm_case_for_group_deleting = api.crm.create_case(random_word).data
+case_for_group_deleting_by_filter = api.crm.create_case(random_word).data
+
 describe '[CRM]' do
   let(:teamlab_module) { :crm }
-
-  describe '#create_person' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :crm }
-      let(:command) { :create_person }
-      let(:args) { [random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :new_contact_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_case' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_case }
-      let(:args) { [random_word, { members: @data_collector[:new_contact_ids] }] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_case_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_case_for_group_deleting' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_case }
-      let(:args) { [random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_case_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_case_for_group_deleting_by_filter' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_case }
-      let(:args) { [random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_case_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
 
   describe '#get_cases_by_prefix' do
     it_behaves_like 'an api request' do
       let(:command) { :get_cases_by_prefix }
-      let(:args) { [random_id(:new_contact), random_word] }
+      let(:args) { [crm_case_user['id'], random_word] }
     end
   end
 
@@ -62,95 +26,91 @@ describe '[CRM]' do
   describe '#add_case_contact' do
     it_behaves_like 'an api request' do
       let(:command) { :add_case_contact }
-      let(:args) { [random_id(:crm_case), random_id(:new_contact)] }
+      let(:args) { [crm_case_for_group_deleting['id'], crm_case_user['id']] }
     end
   end
 
   describe '#get_case_by_id' do
     it_behaves_like 'an api request' do
       let(:command) { :get_case_by_id }
-      let(:args) { [random_id(:crm_case)] }
+      let(:args) { [crm_case['id']] }
     end
   end
 
   describe '#get_all_case_contacts' do
     it_behaves_like 'an api request' do
       let(:command) { :get_all_case_contacts }
-      let(:args) { [random_id(:crm_case)] }
+      let(:args) { [crm_case['id']] }
     end
   end
 
   describe '#set_case_access_rights' do
     it_behaves_like 'an api request' do
       let(:command) { :set_case_access_rights }
-      let(:args) { [random_id(:crm_case), { isPrivate: random_bool }] }
+      let(:args) { [crm_case['id'], { isPrivate: random_bool }] }
     end
   end
 
   describe '#update_case' do
     it_behaves_like 'an api request' do
       let(:command) { :update_case }
-      let(:args) { [random_id(:crm_case), random_word] }
+      let(:args) { [crm_case['id'], random_word] }
     end
   end
 
   describe '#set_case_access_rights_by_filter' do
     it_behaves_like 'an api request' do
       let(:command) { :set_case_access_rights_by_filter }
-      let(:args) { [{ contactId: random_id(:new_contact), isPrivate: random_bool }] }
+      let(:args) { [{ contactId: crm_case_user['id'], isPrivate: random_bool }] }
     end
   end
 
   describe '#close_case' do
     it_behaves_like 'an api request' do
       let(:command) { :close_case }
-      i = -1
-      let(:args) { [@data_collector[:crm_case_ids][i += 1]] }
+      let(:args) { [crm_case_for_group_deleting['id']] }
     end
   end
 
   describe '#resume_case' do
     it_behaves_like 'an api request' do
       let(:command) { :resume_case }
-      i = -1
-      let(:args) { [@data_collector[:crm_case_ids][i += 1]] }
+      let(:args) { [crm_case_for_group_deleting['id']] }
     end
   end
 
   describe '#set_rights_to_case' do
     it_behaves_like 'an api request' do
       let(:command) { :set_rights_to_case }
-      let(:args) { [random_id(:crm_case), { isPrivate: random_bool }] }
+      let(:args) { [crm_case['id'], { isPrivate: random_bool }] }
     end
   end
 
   describe '#delete_case_contact' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_case_contact }
-      i = -1
-      let(:args) { [@data_collector[:crm_case_ids][i += 1], random_id(:new_contact)] }
+      let(:args) { [crm_case['id'], crm_case_user['id']] }
     end
   end
 
   describe '#delete_case' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_case }
-      let(:args) { [@data_collector[:crm_case_ids].shift] }
+      let(:args) { [crm_case['id']] }
     end
   end
 
   describe '#delete_case_group' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_case_group }
-      let(:args) { [@data_collector[:crm_case_ids].pop] }
+      let(:args) { [crm_case_for_group_deleting['id']] }
     end
   end
 
   describe '#delete_case_group_by_filter' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_case_group }
-      i = -1
-      let(:args) { [{ contactId: @data_collector[:new_contact_ids][i += 1] }] }
+      let(:args) { [{ contactId: case_for_group_deleting_by_filter['id'] }] }
     end
   end
 end
