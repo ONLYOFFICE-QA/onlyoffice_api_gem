eaeec99dd80bb0c9aa5dc1fd318cca96dbf2ae57
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-08-17 14:13:22 +0300

    People/Refactoring reassign methods (#311)
    * People/Refactoring reassign methods
    
    * Grammar fix
    
    * Reassign data only to user
    
    * rubocop changes

--- spec/lib/people/people_reassign_spec.rb
+++ spec/lib/people/people_reassign_spec.rb
@@ -2,56 +2,31 @@
 
 require 'spec_helper'
 
+from_user_reassign = api.people.add_user(random_bool, random_email, random_word.capitalize, random_word.capitalize).data
+api.people.change_people_status('2', [from_user_reassign['id']])
+to_user_reassign = api.people.add_user(false, random_email, random_word.capitalize, random_word.capitalize).data
+
 describe '[People] Reassign' do
   let(:teamlab_module) { :people }
-
-  describe 'Preconditions' do
-    describe '#add_user' do
-      it_behaves_like 'an api request' do
-        let(:command) { :add_user }
-        let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-        let(:add_data_to_collector) { true }
-        let(:data_param) { :user_ids }
-        let(:param_names) { %w[id] }
-      end
-    end
-
-    describe '#add_user' do
-      it_behaves_like 'an api request' do
-        let(:command) { :add_user }
-        let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-        let(:add_data_to_collector) { true }
-        let(:data_param) { :user_ids }
-        let(:param_names) { %w[id] }
-      end
-    end
-
-    describe '#change_people_status' do
-      it_behaves_like 'an api request' do
-        let(:command) { :change_people_status }
-        let(:args) { [2, @data_collector[:user_ids].first] }
-      end
-    end
-  end
 
   describe '#start_reassign_user_data' do
     it_behaves_like 'an api request' do
       let(:command) { :start_reassign_user_data }
-      let(:args) { [@data_collector[:user_ids].first, @data_collector[:user_ids].last, false] }
+      let(:args) { [from_user_reassign['id'], to_user_reassign['id'], false] }
     end
   end
 
-  describe '#get_reassign_progress' do
+  describe '#reassign_progress' do
     it_behaves_like 'an api request' do
-      let(:command) { :get_reassign_progress }
-      let(:args) { [@data_collector[:user_ids].first] }
+      let(:command) { :reassign_progress }
+      let(:args) { [from_user_reassign['id']] }
     end
   end
 
   describe '#terminate_reassign' do
     it_behaves_like 'an api request' do
       let(:command) { :terminate_reassign }
-      let(:args) { [@data_collector[:user_ids].first] }
+      let(:args) { [from_user_reassign['id']] }
     end
   end
 end
