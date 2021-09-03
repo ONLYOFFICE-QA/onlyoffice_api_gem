a691dbc766a742eca2c663a4f65b51581fbc3a6c
Olga Nesterova <olganesterova034@gmail.com>
2021-09-03 15:03:47 +0300

    crm contacts issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,9 @@ require_relative 'support/http_data'
 include PortalCleanup
 
 def api
-  @api ||= Teamlab::OnlyofficeApiInstance.new(server: ENV['ONLYOFFICE_API_GEM_TEST_PORTAL'],
-                                              username: ENV['ONLYOFFICE_API_GEM_TEST_USER'],
-                                              password: ENV['ONLYOFFICE_API_GEM_TEST_PASSWORD'])
+  @api ||= Teamlab::OnlyofficeApiInstance.new(server: 'https://olgaapitest12345.teamlab.info',
+                                              username: 'nola4951@mail.ru',
+                                              password: 'qwertyui')
 end
 
 def configure_test_portal
