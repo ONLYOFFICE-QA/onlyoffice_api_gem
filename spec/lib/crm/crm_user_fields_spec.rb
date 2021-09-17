8abc07414fef42e2fbaa0b44f65d332d006fca07
Olga Nesterova <olganesterova034@gmail.com>
2021-09-17 18:30:25 +0300

    add constant

--- spec/lib/crm/crm_user_fields_spec.rb
+++ spec/lib/crm/crm_user_fields_spec.rb
@@ -2,7 +2,8 @@
 
 require 'spec_helper'
 
-new_user_field = api.crm.create_user_field(ENTITY_TYPES.last, random_word, 3).data
+entry_type = 'contact'
+new_user_field = api.crm.create_user_field(entry_type, random_word, 3).data
 custom_field_contact = api.crm.create_person(random_word.capitalize, random_word.capitalize).data
 
 describe '[CRM]' do
@@ -11,49 +12,49 @@ describe '[CRM]' do
   describe '#create_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :create_user_field }
-      let(:args) { [ENTITY_TYPES.first, random_word, 3] }
+      let(:args) { [entry_type, random_word, 3] }
     end
   end
 
   describe '#get_user_field_values' do
     it_behaves_like 'an api request' do
       let(:command) { :get_user_field_values }
-      let(:args) { [ENTITY_TYPES.last, custom_field_contact['id']] }
+      let(:args) { [entry_type, custom_field_contact['id']] }
     end
   end
 
   describe '#get_user_field_list' do
     it_behaves_like 'an api request' do
       let(:command) { :get_user_field_list }
-      let(:args) { [ENTITY_TYPES.last] }
+      let(:args) { [entry_type] }
     end
   end
 
   describe '#set_user_field_value' do
     it_behaves_like 'an api request' do
       let(:command) { :set_user_field_value }
-      let(:args) { [ENTITY_TYPES.last, custom_field_contact['id'], new_user_field['id'], random_word] }
+      let(:args) { [entry_type, custom_field_contact['id'], new_user_field['id'], random_word] }
     end
   end
 
   describe '#update_selected_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :update_selected_user_field }
-      let(:args) { [ENTITY_TYPES.last, new_user_field['id'], random_word, 3] }
+      let(:args) { [entry_type, new_user_field['id'], random_word, 3] }
     end
   end
 
   describe '#update_user_fields_order' do
     it_behaves_like 'an api request' do
       let(:command) { :update_user_fields_order }
-      let(:args) { [ENTITY_TYPES.last, new_user_field['id']] }
+      let(:args) { [entry_type, new_user_field['id']] }
     end
   end
 
   describe '#delete_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_user_field }
-      let(:args) { [ENTITY_TYPES.last, new_user_field['id']] }
+      let(:args) { [entry_type, new_user_field['id']] }
     end
   end
 end
