88b0a2008395b668753deb5ddb5cb6d327393c3e
Olga Nesterova <olganesterova034@gmail.com>
2021-09-17 17:36:28 +0300

    CRM Custom fields refactoring

--- spec/lib/crm/crm_user_fields_spec.rb
+++ spec/lib/crm/crm_user_fields_spec.rb
@@ -2,115 +2,58 @@
 
 require 'spec_helper'
 
+new_user_field = api.crm.create_user_field(ENTITY_TYPES.last, random_word, 3).data
+custom_field_contact = api.crm.create_person(random_word.capitalize, random_word.capitalize).data
+
 describe '[CRM]' do
   let(:teamlab_module) { :crm }
-
-  describe '#add_user' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :people }
-      let(:command) { :add_user }
-      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_person' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :crm }
-      let(:command) { :create_person }
-      let(:args) { [random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :contact_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
 
   describe '#create_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :create_user_field }
-      let(:args) { [ENTITY_TYPES.last, random_word, 3] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_field_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_company' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_company }
-      let(:args) { [random_word, [random_id(:user)]] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :company_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_opportunity_stage' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :crm }
-      let(:command) { :create_opportunity_stage }
-      let(:args) { [random_word.capitalize, COLORS_NAMES.sample] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :opportunity_stage_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_opportunity' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_opportunity }
-      let(:args) { [@data_collector[:opportunity_stage_ids].last, random_word, random_id(:user)] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :opportunity_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [ENTITY_TYPES.first, random_word, 3] }
     end
   end
 
   describe '#get_user_field_values' do
     it_behaves_like 'an api request' do
       let(:command) { :get_user_field_values }
-      let(:args) { [(entity = ENTITY_TYPES.sample).sub('company', 'contact'), random_id(entity.to_sym)] }
+      let(:args) { [ENTITY_TYPES.last, custom_field_contact['id']] }
     end
   end
 
   describe '#get_user_field_list' do
     it_behaves_like 'an api request' do
       let(:command) { :get_user_field_list }
-      let(:args) { [ENTITY_TYPES.sample.sub('company', 'contact')] }
+      let(:args) { [ENTITY_TYPES.last] }
     end
   end
 
   describe '#set_user_field_value' do
     it_behaves_like 'an api request' do
       let(:command) { :set_user_field_value }
-      let(:args) { [ENTITY_TYPES.last, random_id(ENTITY_TYPES.last.to_sym), random_id(:user_field), random_word] }
+      let(:args) { [ENTITY_TYPES.last, custom_field_contact['id'], new_user_field['id'], random_word] }
     end
   end
 
   describe '#update_selected_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :update_selected_user_field }
-      i = -1
-      let(:args) { [ENTITY_TYPES.last, @data_collector[:user_field_ids][i += 1], random_word, 3] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_field_title_ids }
-      let(:param_names) { %w[label] }
+      let(:args) { [ENTITY_TYPES.last, new_user_field['id'], random_word, 3] }
     end
   end
 
   describe '#update_user_fields_order' do
     it_behaves_like 'an api request' do
       let(:command) { :update_user_fields_order }
-      let(:args) { [ENTITY_TYPES.last, @data_collector[:user_field_ids]] }
+      let(:args) { [ENTITY_TYPES.last, new_user_field['id']] }
     end
   end
 
   describe '#delete_user_field' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_user_field }
-      let(:args) { [ENTITY_TYPES.last, @data_collector[:user_field_ids].pop] }
+      let(:args) { [ENTITY_TYPES.last, new_user_field['id']] }
     end
   end
 end
