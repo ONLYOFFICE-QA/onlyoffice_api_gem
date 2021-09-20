05046a4fab18b29855614fc9908c89bb49cd6007
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-09-20 16:00:52 +0300

    CRM History refactoring (#331)

--- spec/lib/crm/crm_history_spec.rb
+++ spec/lib/crm/crm_history_spec.rb
@@ -2,6 +2,11 @@
 
 require 'spec_helper'
 
+history_category_new = api.crm.create_history_category(random_word, random_word).data
+history_category_update = api.crm.create_history_category(random_word, random_word).data
+history_contact = api.crm.create_person(random_word.capitalize, random_word.capitalize).data
+history_event = api.crm.create_event(history_contact['id'], random_word, history_category_new['id']).data
+
 describe '[CRM]' do
   let(:teamlab_module) { :crm }
 
@@ -9,30 +14,13 @@ describe '[CRM]' do
     it_behaves_like 'an api request' do
       let(:command) { :create_history_category }
       let(:args) { [random_word, random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_history_category_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_person' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :crm }
-      let(:command) { :create_person }
-      let(:args) { [random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :new_contact_ids }
-      let(:param_names) { %w[id] }
     end
   end
 
   describe '#create_event' do
     it_behaves_like 'an api request' do
       let(:command) { :create_event }
-      let(:args) { [random_id(:new_contact), random_word, random_id(:crm_history_category)] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_event_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [history_contact['id'], random_word, history_category_new['id']] }
     end
   end
 
@@ -42,29 +30,10 @@ describe '[CRM]' do
     end
   end
 
-  describe '#create_history_category' do
-    it_behaves_like 'an api request' do
-      let(:command) { :create_history_category }
-      let(:args) { [random_word, random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_history_category_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
   describe '#update_history_category' do
     it_behaves_like 'an api request' do
       let(:command) { :update_history_category }
-      let(:args) { [@data_collector[:crm_history_category_ids][1], random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :history_category_titles }
-      let(:param_names) { %w[title] }
-    end
-  end
-
-  describe '#get_all_history_categories' do
-    it_behaves_like 'an api request' do
-      let(:command) { :get_all_history_categories }
+      let(:args) { [history_category_update['id'], random_word] }
     end
   end
 
@@ -77,28 +46,28 @@ describe '[CRM]' do
   describe '#update_history_categories_order' do
     it_behaves_like 'an api request' do
       let(:command) { :update_history_categories_order }
-      let(:args) { [@data_collector[:history_category_titles].shuffle] }
+      let(:args) { [history_category_new['title']] }
     end
   end
 
   describe '#update_history_category_icon' do
     it_behaves_like 'an api request' do
       let(:command) { :update_history_category_icon }
-      let(:args) { [random_id(:crm_history_category), random_word] }
+      let(:args) { [history_category_new['id'], random_word] }
     end
   end
 
   describe '#delete_event_and_related_files' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_event_and_related_files }
-      let(:args) { [@data_collector[:crm_event_ids].pop] }
+      let(:args) { [history_event['id']] }
     end
   end
 
   describe '#delete_history_category' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_history_category }
-      let(:args) { [@data_collector[:crm_history_category_ids].pop] }
+      let(:args) { [history_category_update['id']] }
     end
   end
 end
