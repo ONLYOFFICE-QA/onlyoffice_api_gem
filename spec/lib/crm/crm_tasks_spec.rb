3a8262895d2d7090f4f26fffd5e06833d97977a3
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-09-29 15:58:16 +0300

    CRM Tasks refactoring (#337)
    * CRM Opportunity refactoring
    
    * CRM Tasks refactoring
    
    * Undo

--- spec/lib/crm/crm_tasks_spec.rb
+++ spec/lib/crm/crm_tasks_spec.rb
@@ -2,55 +2,40 @@
 
 require 'spec_helper'
 
+tasks_user = api.people.add_user(false, random_email, random_word.capitalize, random_word.capitalize).data
+first_task_category = api.crm.create_task_category(random_word, random_word).data
+second_task_category = api.crm.create_task_category(random_word, random_word).data
+third_task_category = api.crm.create_task_category(random_word, random_word).data
+task = api.crm.create_task(random_word, DateTime.commercial(2015).to_s, tasks_user['id'], first_task_category['id']).data
+
 describe '[CRM]' do
   let(:teamlab_module) { :crm }
-
-  describe '#add_user' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :people }
-      let(:command) { :add_user }
-      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
 
   describe '#create_task_category' do
     it_behaves_like 'an api request' do
       let(:command) { :create_task_category }
       let(:args) { [random_word, random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_task_category_ids }
-      let(:param_names) { %w[id] }
     end
   end
 
   describe '#update_task_category' do
     it_behaves_like 'an api request' do
       let(:command) { :update_task_category }
-      i = -1
-      let(:args) { [@data_collector[:crm_task_category_ids][i += 1], random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_task_category_titles }
-      let(:param_names) { %w[title] }
+      let(:args) { [third_task_category['id'], random_word] }
     end
   end
 
   describe '#create_task' do
     it_behaves_like 'an api request' do
       let(:command) { :create_task }
-      let(:args) { [random_word, DateTime.commercial(2015).to_s, random_id(:user), random_id(:crm_task_category)] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :crm_task_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [random_word, DateTime.commercial(2015).to_s, tasks_user['id'], first_task_category['id']] }
     end
   end
 
   describe '#update_task' do
     it_behaves_like 'an api request' do
       let(:command) { :update_task }
-      let(:args) { [random_id(:crm_task), random_word, DateTime.commercial(2015).to_s, random_id(:crm_task_category)] }
+      let(:args) { [task['id'], random_word, DateTime.commercial(2015).to_s, second_task_category['id']] }
     end
   end
 
@@ -69,56 +54,56 @@ describe '[CRM]' do
   describe '#get_task_by_id' do
     it_behaves_like 'an api request' do
       let(:command) { :get_task_by_id }
-      let(:args) { [random_id(:crm_task)] }
+      let(:args) { [task['id']] }
     end
   end
 
   describe '#get_task_category' do
     it_behaves_like 'an api request' do
       let(:command) { :get_task_category }
-      let(:args) { [random_id(:crm_task_category)] }
+      let(:args) { [second_task_category['id']] }
     end
   end
 
   describe '#close_task' do
     it_behaves_like 'an api request' do
       let(:command) { :close_task }
-      let(:args) { [random_id(:crm_task)] }
+      let(:args) { [task['id']] }
     end
   end
 
   describe '#resume_task' do
     it_behaves_like 'an api request' do
       let(:command) { :resume_task }
-      let(:args) { [random_id(:crm_task)] }
+      let(:args) { [task['id']] }
     end
   end
 
   describe '#update_task_categories_order' do
     it_behaves_like 'an api request' do
       let(:command) { :update_task_categories_order }
-      let(:args) { [@data_collector[:crm_task_category_titles].shuffle] }
+      let(:args) { [second_task_category['title']] }
     end
   end
 
   describe '#update_task_category_icon' do
     it_behaves_like 'an api request' do
       let(:command) { :update_task_category_icon }
-      let(:args) { [random_id(:crm_task_category), random_word] }
+      let(:args) { [first_task_category['id'], random_word] }
     end
   end
 
   describe '#delete_task' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_task }
-      let(:args) { [@data_collector[:crm_task_ids].pop] }
+      let(:args) { [task['id']] }
     end
   end
 
   describe '#delete_task_category' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_task_category }
-      let(:args) { [@data_collector[:crm_task_category_ids].pop] }
+      let(:args) { [second_task_category['id']] }
     end
   end
 end
