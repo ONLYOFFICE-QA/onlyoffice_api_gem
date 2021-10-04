a9a9381157b0b120f04d78f993a1d8f1fb4cba9d
Olga Nesterova <olganesterova034@gmail.com>
2021-10-04 16:14:33 +0300

    Projects Discussions refactoring

--- spec/lib/project/project_discussions_spec.rb
+++ spec/lib/project/project_discussions_spec.rb
@@ -2,38 +2,18 @@
 
 require 'spec_helper'
 
+discussion_user = api.people.add_user(false, random_email, random_word.capitalize, random_word.capitalize).data
+discussion_project = api.project.create_project(random_word, random_word, discussion_user['id'], random_word(3), false).data
+discussion = api.project.add_message(discussion_project['id'], random_word, random_word, discussion_user['id']).data
+discussion_status = 'Open'
+
 describe '[Project] Discussions' do
   let(:teamlab_module) { :project }
-
-  describe '#add_user' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :people }
-      let(:command) { :add_user }
-      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_project' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :project }
-      let(:command) { :create_project }
-      let(:args) { [random_word, random_word, random_id(:user), random_word(3), random_bool] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :project_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
 
   describe '#add_message' do
     it_behaves_like 'an api request' do
       let(:command) { :add_message }
-      let(:args) { [random_id(:project), random_word, random_word, @data_collector[:user_ids].join(',')] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :discussion_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [discussion_project['id'], random_word, random_word, discussion_user['id']] }
     end
   end
 
@@ -46,42 +26,70 @@ describe '[Project] Discussions' do
   describe '#get_message_by_filter' do
     it_behaves_like 'an api request' do
       let(:command) { :get_message_by_filter }
-      let(:args) { [{ projectId: random_id(:project) }] }
+      let(:args) { [{ projectId: discussion_project['id'] }] }
     end
   end
 
   describe '#get_messages' do
     it_behaves_like 'an api request' do
       let(:command) { :get_messages }
-      let(:args) { [random_id(:project)] }
+      let(:args) { [discussion_project['id']] }
+    end
+  end
+
+  describe '#get_project_message' do
+    it_behaves_like 'an api request' do
+      let(:command) { :get_project_message }
+      let(:args) { [discussion['id']] }
     end
   end
 
   describe '#check_subscription_to_discussion' do
     it_behaves_like 'an api request' do
       let(:command) { :check_subscription_to_discussion }
-      let(:args) { [random_id(:discussion)] }
+      let(:args) { [discussion['id']] }
     end
   end
 
   describe '#update_message' do
     it_behaves_like 'an api request' do
       let(:command) { :update_message }
-      let(:args) { [random_id(:discussion), random_id(:project), random_word, random_word] }
+      let(:args) { [discussion['id'], discussion_project['id'], random_word, random_word] }
     end
   end
 
   describe '#update_message_status' do
     it_behaves_like 'an api request' do
       let(:command) { :update_message_status }
-      let(:args) { [random_id(:discussion), PROJECT_DISCUSSION_STATUSES.sample] }
+      let(:args) { [discussion['id'], discussion_status] }
     end
   end
 
   describe '#subscribe_to_message_action' do
     it_behaves_like 'an api request' do
       let(:command) { :subscribe_to_message_action }
-      let(:args) { [random_id(:discussion)] }
+      let(:args) { [discussion['id']] }
+    end
+  end
+
+  describe '#get_discussion_subscribers' do
+    it_behaves_like 'an api request' do
+      let(:command) { :get_discussion_subscribers }
+      let(:args) { [discussion['id']] }
+    end
+  end
+
+  describe '#delete_message' do
+    it_behaves_like 'an api request' do
+      let(:command) { :delete_message }
+      let(:args) { [discussion['id']] }
+    end
+  end
+
+  describe '#preview_of_discussion_message' do
+    it_behaves_like 'an api request' do
+      let(:command) { :preview_of_discussion_message }
+      let(:args) { ['<!-- This is a discission message -->'] }
     end
   end
 end
