00873043d0670ec8b312c8abcc1d2277511e38d2
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-10-04 12:56:15 +0300

    Projects Comments Refactoring + new methods (#340)
    * CRM Opportunity refactoring
    
    * Projects Comments Refactoring + new methods
    
    * Undo

--- spec/lib/project/project_comments_spec.rb
+++ spec/lib/project/project_comments_spec.rb
@@ -2,103 +2,76 @@
 
 require 'spec_helper'
 
+comments_user = api.people.add_user(false, random_email, random_word.capitalize, random_word.capitalize).data
+comments_project = api.project.create_project(random_word, random_word, comments_user['id'], random_word(3), random_bool).data
+comments_task = api.project.add_task(comments_project['id'], random_word).data
+discussion = api.project.add_message(comments_project['id'], random_word, random_word, comments_user['id']).data
+task_comment = api.project.add_task_comment(comments_task['id'], random_word).data
+type = 'task'
+
 describe '[Project] Comments' do
   let(:teamlab_module) { :project }
-
-  describe '#add_user' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :people }
-      let(:command) { :add_user }
-      let(:args) { [false, random_email, random_word.capitalize, random_word.capitalize] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :user_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#create_project' do
-    it_behaves_like 'an api request' do
-      let(:teamlab_module) { :project }
-      let(:command) { :create_project }
-      let(:args) { [random_word, random_word, random_id(:user), random_word(3), random_bool] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :project_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#add_task' do
-    it_behaves_like 'an api request' do
-      let(:command) { :add_task }
-      let(:args) { [random_id(:project), random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :project_task_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
-
-  describe '#add_message' do
-    it_behaves_like 'an api request' do
-      let(:command) { :add_message }
-      let(:args) { [random_id(:project), random_word, random_word, @data_collector[:user_ids].join(',')] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :discussion_ids }
-      let(:param_names) { %w[id] }
-    end
-  end
 
   describe '#add_task_comment' do
     it_behaves_like 'an api request' do
       let(:command) { :add_task_comment }
-      let(:args) { [random_id(:project_task), random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :comment_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [comments_task['id'], random_word] }
     end
   end
 
   describe '#add_message_comment' do
     it_behaves_like 'an api request' do
       let(:command) { :add_message_comment }
-      let(:args) { [random_id(:discussion), random_word] }
-      let(:add_data_to_collector) { true }
-      let(:data_param) { :comment_ids }
-      let(:param_names) { %w[id] }
+      let(:args) { [discussion['id'], random_word] }
     end
   end
 
   describe '#get_comment' do
     it_behaves_like 'an api request' do
       let(:command) { :get_comment }
-      let(:args) { [random_id(:comment)] }
+      let(:args) { [task_comment['id']] }
     end
   end
 
   describe '#get_task_comments' do
     it_behaves_like 'an api request' do
       let(:command) { :get_task_comments }
-      let(:args) { [random_id(:project_task)] }
+      let(:args) { [comments_task['id']] }
     end
   end
 
   describe '#get_message_comments' do
     it_behaves_like 'an api request' do
       let(:command) { :get_message_comments }
-      let(:args) { [random_id(:discussion)] }
+      let(:args) { [discussion['id']] }
+    end
+  end
+
+  describe '#add_project_comment' do
+    it_behaves_like 'an api request' do
+      let(:command) { :add_project_comment }
+      let(:args) { [comments_task['id'], random_word, type] }
+    end
+  end
+
+  describe '#get_a_comment_preview' do
+    it_behaves_like 'an api request' do
+      let(:command) { :get_a_comment_preview }
+      let(:args) { ['<!-- This is a comment -->'] }
     end
   end
 
   describe '#update_comment' do
     it_behaves_like 'an api request' do
       let(:command) { :update_comment }
-      let(:args) { [random_id(:comment), random_word] }
+      let(:args) { [task_comment['id'], random_word] }
     end
   end
 
   describe '#delete_message_comments' do
     it_behaves_like 'an api request' do
       let(:command) { :delete_comment }
-      let(:args) { [@data_collector[:comment_ids].pop] }
+      let(:args) { [task_comment['id']] }
     end
   end
 end
