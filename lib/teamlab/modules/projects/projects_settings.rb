98c5d64f8f4876bded412cc23710b141090d80f1
Olga Nesterova <olganesterova034@gmail.com>
2021-10-07 13:22:11 +0300

    Add update_projects_settings method for Projects Settings

--- lib/teamlab/modules/projects/projects_settings.rb
+++ lib/teamlab/modules/projects/projects_settings.rb
@@ -7,5 +7,16 @@ module Teamlab
     def projects_settings
       @request.get(%w[settings])
     end
+
+    # Updates the project settings with the parameters specified in the request
+    # @param options [Hash] options to change Projects module settings
+    # @option everebodyCanCreate [Boolean] Specifies if all the portal users can create projects or not
+    # @option hideEntitiesInPausedProjects [Boolean] Specifies if the entities will be hidden in the paused projects or not
+    # @option startModule [String] Module type: Projects, Tasks, Discussions, TimeTracking
+    # @option folderId [String] Folder ID
+    # @return [Hash] Updated settings
+    def update_projects_settings(options = {})
+      @request.put(%w[settings], options)
+    end
   end
 end
--- spec/lib/project/project_settings_spec.rb
+++ spec/lib/project/project_settings_spec.rb
@@ -10,4 +10,11 @@ describe '[Project] Projects' do
       let(:command) { :projects_settings }
     end
   end
+
+  describe '#update_projects_settings' do
+    it_behaves_like 'an api request' do
+      let(:command) { :update_projects_settings }
+      let(:args) { [everebodyCanCreate: false] }
+    end
+  end
 end
