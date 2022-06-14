dfb87dae20268b62e658ca41db5c9eaffdd1e279
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-06-14 13:11:44 +0300

    Updated password for mail account, remove pending (#436)

--- spec/lib/mail/mail_accounts_spec.rb
+++ spec/lib/mail/mail_accounts_spec.rb
@@ -5,17 +5,15 @@ require 'spec_helper'
 describe '[Mail] Accounts' do
   let(:teamlab_module) { :mail }
 
-  describe '#create_account_by_email', :skip do
+  describe '#create_account_by_email' do
     it_behaves_like 'an api request' do
-      skip('Some troubles connecting mail.ru account. Just hang up')
       let(:command) { :create_account_by_email }
       let(:args) { [EMAIL, EMAIL_PASS] }
     end
   end
 
   describe '#set_default_account' do
-    it_behaves_like 'an api request', :skip do
-      skip('Some troubles connecting mail.ru account. Just hang up')
+    it_behaves_like 'an api request' do
       let(:command) { :set_default_account }
       let(:args) { [EMAIL, true] }
     end
